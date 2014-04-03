9fc5ef6079f6c554e6c065fba00a1d165d0cd1db
AdrianTM <AdrianTM@users.noreply.github.com>
2014-04-03 16:20:36 -0400

    execute as root only

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <QApplication>
 #include "mxbootrepair.h"
+#include <qtranslator.h>
+#include <qlocale.h>
+#include <unistd.h>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    mxbootrepair w;
-    w.show();
-    
-    return a.exec();
+    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx/mx-bootrepair.png"));
+
+    QTranslator qtTran;
+    qtTran.load(QString("qt_") + QLocale::system().name());
+    a.installTranslator(&qtTran);
+
+    QTranslator appTran;
+    appTran.load(QString("mx-bootrepair_") + QLocale::system().name(), "/usr/share/mx-bootrepair/locale");
+    a.installTranslator(&appTran);
+
+    if (getuid() == 0) {
+        mxbootrepair w;
+        w.show();
+
+        return a.exec();
+
+    } else {
+        QApplication::beep();
+        QMessageBox::critical(0, QString::null,
+                              QApplication::tr("You must run this program as root."));
+        return 1;
+    }
 }
