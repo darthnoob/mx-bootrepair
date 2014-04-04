e140995382bea88f56420f6e32d75cce6160d60f
Adrian @Home <adilecu@gmail.com>
2014-04-04 10:20:06 -0400

    added license info

--- mxbootrepair.cpp
+++ mxbootrepair.cpp
@@ -1,3 +1,27 @@
+/*****************************************************************************
+ * mxbootrepair.cpp
+ *****************************************************************************
+ * Copyright (C) 2014 MX Authors
+ *
+ * Authors: Adrian
+ *          MEPIS Community <http://forum.mepiscommunity.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+
 #include "mxbootrepair.h"
 #include "ui_mxbootrepair.h"
 
@@ -94,6 +118,7 @@ void mxbootrepair::procDone(int exitCode, QProcess::ExitStatus exitStatus) {
     }
 }
 
+// set proc and timer connections
 void mxbootrepair::setConnections(QTimer* timer, QProcess* proc) {
     disconnect(timer, SIGNAL(timeout()), 0, 0);
     connect(timer, SIGNAL(timeout()), this, SLOT(procTime()));
