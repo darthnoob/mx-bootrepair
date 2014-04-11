3eb6a79a4d0404a4cd02fddb4ca5c2146ff5c3a4
Adrian @Home <adilecu@gmail.com>
2014-04-10 22:04:20 -0400

    update translation strings

--- translations/mx-bootrepair_nl.ts
+++ translations/mx-bootrepair_nl.ts
@@ -13,13 +13,13 @@
     <name>mxbootrepair</name>
     <message>
         <location filename="../mxbootrepair.ui" line="14"/>
-        <location filename="../mxbootrepair.cpp" line="188"/>
+        <location filename="../mxbootrepair.cpp" line="266"/>
         <source>MX Boot Repair</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mxbootrepair.ui" line="31"/>
-        <source>MX Boot Repair is an utility that can be used to reinstall GRUB bootloader on the MBR or root partition. It also provides the option to reconstruct the GRUB configuration file.</source>
+        <source>MX Boot Repair is a utility that can be used to reinstall GRUB bootloader on the MBR (Master Boot Record) or root partition. It provides the option to reconstruct the GRUB configuration file and to back up and restore MBR or PBR.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -29,132 +29,192 @@
     </message>
     <message>
         <location filename="../mxbootrepair.ui" line="62"/>
+        <source>Backup MBR or PBR (legacy boot only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.ui" line="69"/>
         <source>Reinstall GRUB bootloader</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="72"/>
+        <location filename="../mxbootrepair.ui" line="79"/>
         <source>Repair GRUB configuration file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="92"/>
+        <location filename="../mxbootrepair.ui" line="86"/>
+        <source>Restore MBR or PBR from backup (legacy boot only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.ui" line="106"/>
         <source>Select Boot Method</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="145"/>
+        <location filename="../mxbootrepair.ui" line="159"/>
+        <source>Root (Partition Boot Record)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.ui" line="162"/>
         <source>root</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="167"/>
+        <location filename="../mxbootrepair.ui" line="184"/>
+        <source>Master Boot Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.ui" line="187"/>
         <source>MBR</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="170"/>
-        <location filename="../mxbootrepair.ui" line="317"/>
+        <location filename="../mxbootrepair.ui" line="190"/>
+        <location filename="../mxbootrepair.ui" line="337"/>
         <source>Alt+B</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="189"/>
+        <location filename="../mxbootrepair.ui" line="209"/>
         <source>Location:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="211"/>
+        <location filename="../mxbootrepair.ui" line="231"/>
         <source>Install on:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="307"/>
+        <location filename="../mxbootrepair.ui" line="327"/>
         <source>About this application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="310"/>
+        <location filename="../mxbootrepair.ui" line="330"/>
         <source>About...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="359"/>
+        <location filename="../mxbootrepair.ui" line="379"/>
         <source>Display help </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="362"/>
+        <location filename="../mxbootrepair.ui" line="382"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="369"/>
+        <location filename="../mxbootrepair.ui" line="389"/>
         <source>Alt+H</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="382"/>
+        <location filename="../mxbootrepair.ui" line="402"/>
         <source>Apply any changes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="385"/>
+        <location filename="../mxbootrepair.ui" line="405"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="405"/>
+        <location filename="../mxbootrepair.ui" line="425"/>
         <source>Cancel any changes then quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="408"/>
+        <location filename="../mxbootrepair.ui" line="428"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.ui" line="415"/>
+        <location filename="../mxbootrepair.ui" line="435"/>
         <source>Alt+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="105"/>
+        <location filename="../mxbootrepair.cpp" line="112"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="113"/>
+        <source>You are going to write the content of </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="113"/>
+        <source> to </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="113"/>
+        <source>
+
+Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="147"/>
         <source>Success</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="106"/>
+        <location filename="../mxbootrepair.cpp" line="148"/>
         <source>Process finished with success.&lt;p&gt;&lt;b&gt;Do you want to exit MX Boot Repair?&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="114"/>
+        <location filename="../mxbootrepair.cpp" line="156"/>
+        <location filename="../mxbootrepair.cpp" line="241"/>
+        <location filename="../mxbootrepair.cpp" line="248"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="115"/>
+        <location filename="../mxbootrepair.cpp" line="157"/>
         <source>Process finished. Errors have occurred.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="187"/>
+        <location filename="../mxbootrepair.cpp" line="239"/>
+        <source>Select backup file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="241"/>
+        <location filename="../mxbootrepair.cpp" line="248"/>
+        <source>No file was selected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="246"/>
+        <source>Select MBR or PBR backup file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxbootrepair.cpp" line="265"/>
         <source>About MX Boot Repair</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="189"/>
+        <location filename="../mxbootrepair.cpp" line="267"/>
         <source>Simple boot repair program for antiX MX</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="190"/>
+        <location filename="../mxbootrepair.cpp" line="268"/>
         <source>Copyright (c) antiX</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mxbootrepair.cpp" line="191"/>
+        <location filename="../mxbootrepair.cpp" line="269"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
