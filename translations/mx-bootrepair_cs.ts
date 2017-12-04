729326cb40cab60de3237f138ce0c711747ad5c5
Adrian <adrian@mxlinux.org>
2017-12-04 14:28:56 -0500

    build with new translations

--- translations/mx-bootrepair_cs.ts
+++ translations/mx-bootrepair_cs.ts
@@ -1,8 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="main.cpp" line="53"/>
+        <location filename="../main.cpp" line="53"/>
         <source>You must run this program as root.</source>
         <translation>Tuto aplikaci musíte spustit jako root.</translation>
     </message>
@@ -10,196 +12,196 @@
 <context>
     <name>mxbootrepair</name>
     <message>
-        <location filename="mxbootrepair.ui" line="20"/>
-        <location filename="mxbootrepair.cpp" line="447"/>
+        <location filename="../mxbootrepair.ui" line="20"/>
+        <location filename="../mxbootrepair.cpp" line="447"/>
         <source>MX Boot Repair</source>
         <translation>MX Oprava zavaděče</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="39"/>
+        <location filename="../mxbootrepair.ui" line="39"/>
         <source>MX Boot Repair is a utility that can be used to reinstall GRUB bootloader on the MBR (Master Boot Record) or root partition. It provides the option to reconstruct the GRUB configuration file and to back up and restore MBR or PBR.</source>
         <translation>MX Oprava zavaděče je aplikace, která může být použita pro přeinstalování GRUB zavaděče uloženého v MBR (Master Boot Record) nebo root oddílu. Program poskutyje volbu rekonstrukce konfiguračného souboru GRUB-u a zálohování/obnovení MBR nebo PBR.</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="58"/>
+        <location filename="../mxbootrepair.ui" line="58"/>
         <source>What would you like to do?</source>
         <translation>Co si přejete provést?</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="79"/>
+        <location filename="../mxbootrepair.ui" line="79"/>
         <source>Backup MBR or PBR (legacy boot only)</source>
         <translation>Záloha MBR a PBR (pouze pro legacy boot)</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="86"/>
+        <location filename="../mxbootrepair.ui" line="86"/>
         <source>Reinstall GRUB bootloader</source>
         <translation>Přeinstalovat zavaděč GRUB</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="96"/>
+        <location filename="../mxbootrepair.ui" line="96"/>
         <source>Repair GRUB configuration file</source>
         <translation>Opravit konfigurační soubor GRUB-u</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="103"/>
+        <location filename="../mxbootrepair.ui" line="103"/>
         <source>Restore MBR or PBR from backup (legacy boot only)</source>
         <translation>Obnovit MBR nebo PBR ze zálohy (pouze pro legacy boot)</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="123"/>
+        <location filename="../mxbootrepair.ui" line="123"/>
         <source>Select Boot Method</source>
         <translation>Vyberte způsob zavedení</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="141"/>
+        <location filename="../mxbootrepair.ui" line="141"/>
         <source>Master Boot Record</source>
         <translation>Master Boot Record</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="144"/>
+        <location filename="../mxbootrepair.ui" line="144"/>
         <source>MBR</source>
         <translation>MBR</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="147"/>
-        <location filename="mxbootrepair.ui" line="418"/>
+        <location filename="../mxbootrepair.ui" line="147"/>
+        <location filename="../mxbootrepair.ui" line="418"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="169"/>
+        <location filename="../mxbootrepair.ui" line="169"/>
         <source>Root (Partition Boot Record)</source>
         <translation>Root (Partition Boot Record)</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="172"/>
+        <location filename="../mxbootrepair.ui" line="172"/>
         <source>root</source>
         <translation>root</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="223"/>
+        <location filename="../mxbootrepair.ui" line="223"/>
         <source>Install on:</source>
         <translation>Instalovat na:</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="242"/>
+        <location filename="../mxbootrepair.ui" line="242"/>
         <source>Location:</source>
         <translation>Umístnění:</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="264"/>
+        <location filename="../mxbootrepair.ui" line="264"/>
         <source>Select /boot location:</source>
         <translation>Vyberte umístnění /boot:</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="277"/>
+        <location filename="../mxbootrepair.ui" line="277"/>
         <source>EFI System Partition</source>
         <translation>Oddíl systému EFI</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="280"/>
+        <location filename="../mxbootrepair.ui" line="280"/>
         <source>ESP</source>
         <translation>ESP</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="407"/>
+        <location filename="../mxbootrepair.ui" line="407"/>
         <source>About this application</source>
         <translation>O této aplikaci</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="410"/>
+        <location filename="../mxbootrepair.ui" line="410"/>
         <source>About...</source>
         <translation>O aplikaci ...</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="460"/>
+        <location filename="../mxbootrepair.ui" line="460"/>
         <source>Display help </source>
         <translation>Zobrazit nápovědu</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="463"/>
+        <location filename="../mxbootrepair.ui" line="463"/>
         <source>Help</source>
         <translation>Nápověda</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="471"/>
+        <location filename="../mxbootrepair.ui" line="471"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="512"/>
+        <location filename="../mxbootrepair.ui" line="512"/>
         <source>Cancel any changes then quit</source>
         <translation>Zrušit všechny změny a ukončit</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="515"/>
-        <location filename="mxbootrepair.cpp" line="452"/>
+        <location filename="../mxbootrepair.ui" line="515"/>
+        <location filename="../mxbootrepair.cpp" line="452"/>
         <source>Cancel</source>
         <translation>Zrušit</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="523"/>
+        <location filename="../mxbootrepair.ui" line="523"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="542"/>
+        <location filename="../mxbootrepair.ui" line="542"/>
         <source>Apply any changes</source>
         <translation>Uložit všechny změny</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.ui" line="545"/>
-        <location filename="mxbootrepair.cpp" line="77"/>
+        <location filename="../mxbootrepair.ui" line="545"/>
+        <location filename="../mxbootrepair.cpp" line="77"/>
         <source>Apply</source>
         <translation>Použít</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="110"/>
-        <location filename="mxbootrepair.cpp" line="133"/>
-        <location filename="mxbootrepair.cpp" line="163"/>
-        <location filename="mxbootrepair.cpp" line="265"/>
-        <location filename="mxbootrepair.cpp" line="299"/>
-        <location filename="mxbootrepair.cpp" line="404"/>
-        <location filename="mxbootrepair.cpp" line="420"/>
-        <location filename="mxbootrepair.cpp" line="427"/>
+        <location filename="../mxbootrepair.cpp" line="110"/>
+        <location filename="../mxbootrepair.cpp" line="133"/>
+        <location filename="../mxbootrepair.cpp" line="163"/>
+        <location filename="../mxbootrepair.cpp" line="265"/>
+        <location filename="../mxbootrepair.cpp" line="299"/>
+        <location filename="../mxbootrepair.cpp" line="404"/>
+        <location filename="../mxbootrepair.cpp" line="420"/>
+        <location filename="../mxbootrepair.cpp" line="427"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="111"/>
+        <location filename="../mxbootrepair.cpp" line="111"/>
         <source>Could not mount </source>
         <translation>Nelze připojit</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="111"/>
+        <location filename="../mxbootrepair.cpp" line="111"/>
         <source> on /boot/efi</source>
         <translation>na /boot/efi</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="134"/>
-        <location filename="mxbootrepair.cpp" line="164"/>
+        <location filename="../mxbootrepair.cpp" line="134"/>
+        <location filename="../mxbootrepair.cpp" line="164"/>
         <source>Could not set up chroot environment.
 Please double-check the selected location.</source>
         <translation>Nebylo možné nastavit chroot prostředí.
 Prosím zkontrolujte vybrané umístnění.</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="240"/>
+        <location filename="../mxbootrepair.cpp" line="240"/>
         <source>Warning</source>
         <translation>Varování</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="241"/>
+        <location filename="../mxbootrepair.cpp" line="241"/>
         <source>You are going to write the content of </source>
         <translation>Chystáte se zapsat obsah</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="241"/>
+        <location filename="../mxbootrepair.cpp" line="241"/>
         <source> to </source>
         <translation>do</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="241"/>
+        <location filename="../mxbootrepair.cpp" line="241"/>
         <source>
 
 Are you sure?</source>
@@ -208,91 +210,91 @@ Are you sure?</source>
 Jste si jistí?</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="242"/>
-        <location filename="mxbootrepair.cpp" line="295"/>
+        <location filename="../mxbootrepair.cpp" line="242"/>
+        <location filename="../mxbootrepair.cpp" line="295"/>
         <source>Yes</source>
         <translation>Ano</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="242"/>
-        <location filename="mxbootrepair.cpp" line="295"/>
+        <location filename="../mxbootrepair.cpp" line="242"/>
+        <location filename="../mxbootrepair.cpp" line="295"/>
         <source>No</source>
         <translation>Ne</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="266"/>
+        <location filename="../mxbootrepair.cpp" line="266"/>
         <source>Could not find EFI system partition (ESP) on any system disks. Please create an ESP and try again.</source>
         <translation>Nelze najít EFI oddíl (ESP) na systémových discích. Prosím vytvořte ESP a zkuste to znovu.</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="293"/>
+        <location filename="../mxbootrepair.cpp" line="293"/>
         <source>Success</source>
         <translation>Úspěch!</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="294"/>
+        <location filename="../mxbootrepair.cpp" line="294"/>
         <source>Process finished with success.&lt;p&gt;&lt;b&gt;Do you want to exit MX Boot Repair?&lt;/b&gt;</source>
         <translation>Proces byl úspěšně dokončen.&lt;p&gt;&lt;b&gt;Přejete si ukončit program MX Oprava zavaděče?&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="300"/>
-        <location filename="mxbootrepair.cpp" line="405"/>
+        <location filename="../mxbootrepair.cpp" line="300"/>
+        <location filename="../mxbootrepair.cpp" line="405"/>
         <source>Process finished. Errors have occurred.</source>
         <translation>Proces dokončen. Vyskytly se chyby. </translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="302"/>
+        <location filename="../mxbootrepair.cpp" line="302"/>
         <source>Back</source>
         <translation>Předchozí</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="418"/>
+        <location filename="../mxbootrepair.cpp" line="418"/>
         <source>Select backup file name</source>
         <translation>Vyberte název souboru zálohy</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="420"/>
-        <location filename="mxbootrepair.cpp" line="427"/>
+        <location filename="../mxbootrepair.cpp" line="420"/>
+        <location filename="../mxbootrepair.cpp" line="427"/>
         <source>No file was selected.</source>
         <translation>Nebyl vybrán žádný soubor</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="425"/>
+        <location filename="../mxbootrepair.cpp" line="425"/>
         <source>Select MBR or PBR backup file</source>
         <translation>Vyberte soubor MBR nebo PBR zálohy</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="446"/>
+        <location filename="../mxbootrepair.cpp" line="446"/>
         <source>About MX Boot Repair</source>
         <translation>O programu MX Oprava zavaděče</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="447"/>
+        <location filename="../mxbootrepair.cpp" line="447"/>
         <source>Version: </source>
         <translation>Verze:</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="449"/>
+        <location filename="../mxbootrepair.cpp" line="449"/>
         <source>Simple boot repair program for MX Linux</source>
         <translation>Jednoduchá oprava zavaděče pro MX Linux</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="450"/>
+        <location filename="../mxbootrepair.cpp" line="450"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Vlastnická práva (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="451"/>
+        <location filename="../mxbootrepair.cpp" line="451"/>
         <source>License</source>
         <translation>Licence</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="454"/>
+        <location filename="../mxbootrepair.cpp" line="454"/>
         <source>&apos;MX Boot Repair License&apos;</source>
         <translation>&apos;Licence MX Oprava zavaděče&apos;</translation>
     </message>
     <message>
-        <location filename="mxbootrepair.cpp" line="470"/>
+        <location filename="../mxbootrepair.cpp" line="470"/>
         <source>MX Boot Repair Help</source>
         <translation>Pomoc MX Oprava zavaděče</translation>
     </message>
